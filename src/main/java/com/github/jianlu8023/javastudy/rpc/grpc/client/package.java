9313caa42cc9d4d8c517997b9328ca0264ebd7f8
jianlu <jianlu@private.com>
2024-04-07 13:25:43 +0800

    dir fix

--- src/main/java/com/github/jianlu8023/javastudy/rpc/grpc/client/package.java
+++
@@ -1,3 +0,0 @@
-package com.github.jianlu8023.javastudy.rpc.grpc.client;
-
-// 在web包体现 controller service 等子包
--- src/main/java/com/github/jianlu8023/javastudy/web/service/DaMengService.java
+++
@@ -1,8 +0,0 @@
-package com.github.jianlu8023.javastudy.web.service;
-
-import java.util.*;
-
-public interface DaMengService {
-
-    List<Map<String, Object>> queryDbVersion();
-}
