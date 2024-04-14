ac92be7ee86faba74e1f718fb47bad3ad6cd4c57
jianlu <jianlu@private.com>
2024-04-14 12:28:34 +0800

    logging

---
+++ java-demo-logging/src/main/java/com/github/jianlu8023/example/logging/LoggingApplication.java
@@ -0,0 +1,11 @@
+package com.github.jianlu8023.example.logging;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class LoggingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoggingApplication.class, args);
+    }
+}
