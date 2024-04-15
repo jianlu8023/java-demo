7631b76bc06b953e66bd470ff843c80a36ffcab9
jianlu <jianlu@private.com>
2024-04-15 21:57:41 +0800

    mock

---
+++ java-demo-mock/src/main/java/com/github/jianlu8023/utils/config/GeneratorAutoConfiguration.java
@@ -0,0 +1,11 @@
+package com.github.jianlu8023.utils.config;
+
+import lombok.extern.slf4j.*;
+import org.springframework.context.annotation.*;
+
+@Configuration
+@Slf4j
+public class GeneratorAutoConfiguration {
+
+
+}
---
+++ java-demo-mock/src/main/java/com/github/jianlu8023/utils/generator/Generator.java
@@ -0,0 +1,13 @@
+package com.github.jianlu8023.utils.generator;
+
+import java.util.*;
+
+public abstract class Generator {
+
+    abstract String generator();
+
+    private Random random = null;
+
+
+
+}
