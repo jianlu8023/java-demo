e5108dbc55de0e27bbd04577a1bbe805bc578ed8
jianlu <jianlu@private.com>
2024-04-15 16:45:34 +0800

    logging

--- java-demo-logging/src/main/java/com/github/jianlu8023/example/logging/LoggingApplication.java
+++ java-demo-logging/src/main/java/com/github/jianlu8023/example/logging/LoggingApplication.java
@@ -2,8 +2,10 @@ package com.github.jianlu8023.example.logging;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.scheduling.annotation.*;
 
 @SpringBootApplication
+@EnableScheduling
 public class LoggingApplication {
     public static void main(String[] args) {
         SpringApplication.run(LoggingApplication.class, args);
