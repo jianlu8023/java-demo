adef0e8e21a3bd688bfbb4a042426afb2b8303aa
jianlu <jianlu@private.com>
2024-05-17 13:34:49 +0800

    redis init

---
+++ java-demo-redis/src/main/java/com/github/jianlu8023/example/javademoredis/JavaDemoRedisApplication.java
@@ -0,0 +1,13 @@
+package com.github.jianlu8023.example.javademoredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDemoRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDemoRedisApplication.class, args);
+    }
+
+}
