c62d753f24677a4efebc39b6ee8a2300760d6a0c
jianlu <jianlu8023@gmail.com>
2024-09-07 00:42:16 +0800

    mybatisplus-batchinsert 调整

--- java-demo-mybatisplus-batchinsert/src/main/java/com/github/jianlu8023/example/mybatisplus/batchinsert/mybatisplus/injector/BatchInsertSqlInjector.java
+++ java-demo-mybatisplus-batchinsert/src/main/java/com/github/jianlu8023/example/mybatisplus/batchinsert/mybatisplus/injector/BatchInsertSqlInjector.java
@@ -9,16 +9,7 @@ import java.util.*;
 
 
 public class BatchInsertSqlInjector extends DefaultSqlInjector {
-    /**
-     * getMethodList
-     * <p>
-     * create time: 2023/12/5 下午5:50
-     * create by: jianlu8023
-     *
-     * @param mapperClass :
-     * @param tableInfo   :
-     * @return java.util.List<com.baomidou.mybatisplus.core.injector.AbstractMethod>
-     */
+
     @Override
     public List<AbstractMethod> getMethodList(Class<?> mapperClass, TableInfo tableInfo) {
         List<AbstractMethod> methodList = super.getMethodList(mapperClass, tableInfo);
