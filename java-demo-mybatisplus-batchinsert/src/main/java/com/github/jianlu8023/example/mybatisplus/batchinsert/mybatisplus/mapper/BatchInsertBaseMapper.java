527c6f0f5dd08b865c5a7c3f0a5db000647f88f4
jianlu <jianlu8023@gmail.com>
2024-09-07 00:42:28 +0800

    mybatisplus-batchinsert 调整

--- java-demo-mybatisplus-batchinsert/src/main/java/com/github/jianlu8023/example/mybatisplus/batchinsert/mybatisplus/mapper/BatchInsertBaseMapper.java
+++ java-demo-mybatisplus-batchinsert/src/main/java/com/github/jianlu8023/example/mybatisplus/batchinsert/mybatisplus/mapper/BatchInsertBaseMapper.java
@@ -6,14 +6,6 @@ import java.util.*;
 
 public interface BatchInsertBaseMapper<T> extends BaseMapper<T> {
 
-    /**
-     * insertBatchSomeColumn
-     * <p>
-     * create time: 2023/12/5 下午5:48
-     * create by: jianlu8023
-     *
-     * @param batchList :
-     * @return java.lang.Integer
-     */
+
     Integer insertBatchSomeColumn(Collection<T> batchList);
 }
