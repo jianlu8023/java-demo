a066b9694f69cd173887697ee3ed88d259bb03be
jianlu <jianlu8023@gmail.com>
2024-09-14 12:34:41 +0800

    integration redis

---
+++ java-demo-integration/src/main/java/com/github/jianlu8023/example/integration/redis/config/RedisConfiguration.java
@@ -0,0 +1,6 @@
+package com.github.jianlu8023.example.integration.redis.config;
+
+
+
+public class RedisConfiguration {
+}
---
+++ java-demo-integration/src/main/java/com/github/jianlu8023/example/integration/redis/package.java
@@ -0,0 +1,3 @@
+package com.github.jianlu8023.example.integration.redis;
+
+// https://cloud.tencent.com/developer/article/1975743
