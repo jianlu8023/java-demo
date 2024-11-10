8bbbdaf9d40a145b533cbb3449f82d5c2b021a5f
jianlu <jianlu8023@gmail.com>
2024-11-10 12:51:04 +0800

    调整

--- java-demo-integration/src/main/java/com/github/jianlu8023/example/integration/middleware/utils/SSLUtils.java
+++ java-demo-integration/src/main/java/com/github/jianlu8023/example/integration/middleware/utils/SSLUtils.java
@@ -9,20 +9,7 @@ public class SSLUtils {
     public static SSLContext createSSLContext() {
         try {
             SSLContext sslContext = SSLContext.getInstance("SSL");
-            sslContext.init(null, new TrustManager[]{new X509TrustManager() {
-                @Override
-                public void checkClientTrusted(X509Certificate[] x509Certificates, String s) {
-                }
-
-                @Override
-                public void checkServerTrusted(X509Certificate[] x509Certificates, String s) {
-                }
-
-                @Override
-                public X509Certificate[] getAcceptedIssuers() {
-                    return new X509Certificate[0];
-                }
-            }}, new SecureRandom());
+            sslContext.init(null, getTrustAllCerts(), new SecureRandom());
             return sslContext;
         } catch (Exception e) {
             throw new RuntimeException(e);
